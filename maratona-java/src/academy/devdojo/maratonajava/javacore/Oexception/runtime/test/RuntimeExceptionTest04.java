4e69a548133cc54522ab25e8b96b4d400e578136
Leonardo Campos <leonardu03@gmail.com>
2022-06-06 10:08:59 -0300

    "Aula 103 concluida"

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class RuntimeExceptionTest04 {
+    public static void main(String[] args) {
+        try {
+            throw new RuntimeException();
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException  e) {
+            System.out.println("Dentro do IndexOutOfBoundsException | IllegalArgumentException | ArithmeticException");
+        } catch (RuntimeException e) {
+            System.out.println("Dentro do RuntimeException");
+        }
+
+        try {
+            talvezLanceException();
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void talvezLanceException() throws IOException, SQLException {
+
+    }
+
+}
+//            Excessões mais genericas, sempre tem que vir no final do código
