52b2d70442872aaf3d6787a85f78731d280310a2
Leonardo Campos <leonardu03@gmail.com>
2022-06-09 14:25:40 -0300

    "Aula 127 concluida"

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Rdatas/ChronoUnitTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Rdatas;
+
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+public class ChronoUnitTest01 {
+    public static void main(String[] args) {
+        LocalDateTime aniversario = LocalDateTime.of(1999, Month.SEPTEMBER, 3, 10,00,00);
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(ChronoUnit.DAYS.between(aniversario, now));
+        System.out.println(ChronoUnit.MONTHS.between(aniversario, now));
+        System.out.println(ChronoUnit.YEARS.between(aniversario, now));
+
+    }
+}
