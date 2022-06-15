d4f5b554351140f49495b4101e7d685ec6c9744d
Leonardo Campos <leonardu03@gmail.com>
2022-06-15 12:23:12 -0300

    "Aula 158 concluida"

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Wnio/test/PathMatcherTest01.java
@@ -0,0 +1,31 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
+
+public class PathMatcherTest01 {
+    public static void main(String[] args) {
+        Path path1 = Paths.get("pasta/subpasta/file.bkp");
+        Path path2 = Paths.get("pasta/subpasta/file.txt");
+        Path path3 = Paths.get("pasta/subpasta/file.java");
+        matches(path1,"glob:*.bkp");
+        matches(path1,"glob:**/*.bkp"); //**/* significa ignorar tudo que tem para tras do .
+        matches(path1,"glob:**/*.{bkp,txt,java}"); // sintaxe para procurar todos os arquivos referenciados (nao colocar espaço)
+        matches(path2,"glob:**/*.{bkp,txt,java}");
+        matches(path3,"glob:**/*.{bkp,txt,java}");
+        matches(path1,"glob:**/*.???"); // sintaxe para procurar .??? caracteres, por isso path3 deu false
+        matches(path2,"glob:**/*.???");
+        matches(path3,"glob:**/*.???");
+        matches(path3,"glob:**/file.????"); // pesquisar por nome especifico
+
+
+
+    }
+
+    private static void matches(Path path, String glob){
+        PathMatcher matcher = FileSystems.getDefault().getPathMatcher(glob);
+        System.out.println(glob + ": "+matcher.matches(path));
+    }
+}
