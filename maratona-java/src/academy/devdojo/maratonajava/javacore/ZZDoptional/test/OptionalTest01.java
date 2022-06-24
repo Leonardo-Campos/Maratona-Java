d4b57c1561dc2bbe7e4a98b23f9791694846646c
Leonardo Campos <leonardu03@gmail.com>
2022-06-24 15:09:27 -0300

    "Aula 203 concluida"

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZDoptional/test/OptionalTest01.java
@@ -0,0 +1,33 @@
+package academy.devdojo.maratonajava.javacore.ZZDoptional.test;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+
+public class OptionalTest01 {
+    public static void main(String[] args) {
+        Optional<String> o1 = Optional.of("Aha o Devdojo é zika");
+        Optional<String> o2 = Optional.ofNullable(null);
+        Optional<String> o3 = Optional.empty();
+
+        System.out.println(o1);
+        System.out.println(o2);
+        System.out.println(o3);
+        System.out.println("-----");
+
+        Optional<String> nameOptional = findName("William");
+        String empty = nameOptional.orElse("EMPTY");
+        nameOptional.ifPresent(s -> System.out.println(s.toUpperCase()));
+        System.out.println(empty);
+
+    }
+
+    private static Optional<String> findName(String name){
+        List<String> list = List.of("William", "Leonzin");
+        int i = list.indexOf(name);
+        if(i != 1){
+            return Optional.of(list.get(i));
+        }
+        return Optional.empty();
+    }
+}
---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest01.java
@@ -0,0 +1,38 @@
+package academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+
+import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+// Ordenar todos lightnovel by title
+// Retrieve the first 3 titles light novels with price less than 4
+public class StreamTest01 {
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shitara", 8.99),
+            new LightNovel("Overlord", 3.99),
+            new LightNovel("Violet Evergarden", 5.99),
+            new LightNovel("No game no Life", 2.99),
+            new LightNovel("Fullmetal Alchimist", 5.99),
+            new LightNovel("Kumo desuga", 1.99),
+            new LightNovel("Monogatari", 4.00)
+    ));
+    public static void main(String[] args) {
+        lightNovels.sort(Comparator.comparing(LightNovel::getTitle));
+        List<String> titles = new ArrayList<>();
+        for (LightNovel lightNovel : lightNovels) {
+            if(lightNovel.getPrice() <= 4){
+                titles.add(lightNovel.getTitle());
+            }
+            if(titles.size() >= 3){
+                break;
+            }
+        }
+
+        System.out.println(lightNovels);
+        System.out.println(titles);
+    }
+
+}
