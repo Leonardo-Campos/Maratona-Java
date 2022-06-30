f26775716fd0c6275fe58611632f76282918f46a
Leonardo Campos <leonardu03@gmail.com>
2022-06-30 16:48:15 -0300

    "Aula 225 concluida"

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZFthreads/test/ThreadTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZFthreads/test/ThreadTest01.java
@@ -1,13 +1,71 @@
 package academy.devdojo.maratonajava.javacore.ZZFthreads.test;
 
+
+class ThreadExample extends Thread {
+    private char c;
+
+    public ThreadExample(char c) {
+        this.c = c;
+    }
+
+    @Override
+    public void run() {
+        System.out.print(Thread.currentThread().getName());
+        for (int i = 0; i < 500; i++) {
+            System.out.print(c);
+            if (i % 100 == 0) {
+                System.out.println();
+            }
+
+        }
+    }
+}
+
+class ThreadExampleRunnable implements Runnable {
+
+    private char c;
+
+    public ThreadExampleRunnable(char c) {
+        this.c = c;
+    }
+
+    @Override
+    public void run() {
+        System.out.print(Thread.currentThread().getName());
+        for (int i = 0; i < 500; i++) {
+            System.out.print(c);
+            if (i % 100 == 0) {
+                System.out.println();
+            }
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+// Daemon x User
 public class ThreadTest01 {
 
     public static void main(String[] args) {
-        System.out.println(Runtime.getRuntime().availableProcessors());
-        long num = 10_000_000;
+//        ThreadExample t1 = new ThreadExample('A');
+//        ThreadExample t2 = new ThreadExample('B');
+//        ThreadExample t3 = new ThreadExample('C');
+//        ThreadExample t4 = new ThreadExample('D');
 
+        Thread t1 = new Thread(new ThreadExampleRunnable('A'),"T1A");
+        Thread t2 = new Thread(new ThreadExampleRunnable('B'), "T2B");
+        Thread t3 = new Thread(new ThreadExampleRunnable('C'), "T3C");
+        Thread t4 = new Thread(new ThreadExampleRunnable('D'), "T4D");
+
+        t4.setPriority(Thread.MAX_PRIORITY); // geralmente vai de 1 à 10 - não é garantido que será a prioridade na hora da execução
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
     }
-    private static void sumFor(long num){
-        System.out.println("Sum for");
-    }
+
 }
