2385ea909114afe427d0b7449f6773a04409c038
Leonardo Campos <leonardu03@gmail.com>
2022-07-01 15:48:36 -0300

    "Aula 228 concluida"

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZFthreads/service/EmailsDeliveryService.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.ZZFthreads.service;
+
+import academy.devdojo.maratonajava.javacore.ZZFthreads.dominio.Members;
+
+public class EmailsDeliveryService implements Runnable{
+    private final Members members;
+
+    public EmailsDeliveryService(Members members) {
+        this.members = members;
+    }
+
+
+    @Override
+    public void run() {
+        String threadName = Thread.currentThread().getName();
+        System.out.println(threadName+" Starting to delivery emails...");
+        while (members.isOpen() || members.pendingEmails() > 0){
+            try {
+                String email = members.retrieveEmail();
+                if(email == null) continue;
+                System.out.println(threadName+" Enviando email para "+email);
+                Thread.sleep(2000);
+                System.out.println(threadName+" Enviou e-mail com sucesso para "+email);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Todos os emails foram enviados com sucesso");
+    }
+}
