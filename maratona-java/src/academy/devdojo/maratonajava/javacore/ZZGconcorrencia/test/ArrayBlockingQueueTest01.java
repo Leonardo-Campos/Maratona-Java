322f6504e032a7c8f6728d64340eec1aef5f197e
Leonardo Campos <leonardu03@gmail.com>
2022-07-05 16:50:20 -0300

    "Aula 238 concluida"

--- maratona-java/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/ArrayBlockingQueueTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/ZZGconcorrencia/test/ArrayBlockingQueueTest01.java
@@ -1,8 +1,6 @@
 package academy.devdojo.maratonajava.javacore.ZZGconcorrencia.test;
 
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingDeque;
-<<<<<<< HEAD
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
 
@@ -10,17 +8,16 @@ public class ArrayBlockingQueueTest01 {
     public static void main(String[] args) throws InterruptedException {
         BlockingQueue<String> bq = new ArrayBlockingQueue<>(1);
         bq.put("William");
-        System.out.printf("%s added the value %s%n", Thread.currentThread().getName(),bq.peek());
+        System.out.printf("%s added the value %s%n", Thread.currentThread().getName(), bq.peek());
         System.out.println("Trying to add another value");
         new Thread((new RemoveFromQueue(bq))).start();
         bq.put("Suane");
-        System.out.printf("%s added the value %s%n", Thread.currentThread().getName(),bq.peek());
-
+        System.out.printf("%s added the value %s%n", Thread.currentThread().getName(), bq.peek());
 
 
     }
 
-    static class RemoveFromQueue implements Runnable{
+    static class RemoveFromQueue implements Runnable {
         private final BlockingQueue<String> bq;
 
         public RemoveFromQueue(BlockingQueue<String> bq) {
@@ -32,17 +29,10 @@ public class ArrayBlockingQueueTest01 {
             System.out.printf("%s going to sleep for 2s %n", Thread.currentThread().getName());
             try {
                 TimeUnit.SECONDS.sleep(5);
-                System.out.printf("%s removing value from queue %s%n", Thread.currentThread().getName(),bq.take());
+                System.out.printf("%s removing value from queue %s%n", Thread.currentThread().getName(), bq.take());
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-=======
-
-public class ArrayBlockingQueueTest01 {
-    public static void main(String[] args) throws InterruptedException {
-        BlockingDeque<String> bq = new ArrayBlockingQueue<>(1);
-        bq.put("William");
->>>>>>> origin/main
     }
 }
